57ea75e8dbf2029950cb51342d96f766adb10dc3
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-04 17:04:06 +0000

    OO finish

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+// Create a new tsk by adding to the arrays
+// A new tsk will be created as incom
+function newtsk(title, des) {
+  const tsk = {
+  "title": title,
+  "description": des,
+  "complete": false,
+  "logState": function() {
+    console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+  "markCom": function() {
+    this.complete = true;
+    }
+  }
+  return tsk;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+const task1 = newtsk("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newtsk("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+task1.logState(); // Clean Cat Litter has not been comd
+task1.markCom();
+task1.logState(); // Clean Cat Litter has been comd
 
-// DRIVER CODE BELOW
-
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(tasks)
